7952510918a92ec15dcd172e9b39d304337cca9b
Sreekesh K Prabhu <145774649+sreekesh-k@users.noreply.github.com>
2024-11-27 19:36:58 +0530

    pad

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '/sms', // Exclude this URL
+        'chat-app/sms', // Exclude this URL
 
     ];
 }
